74a2d70b451f88570d99bb9b16a4729f416d104c
Cameron <cam.hurd@gmail.com>
2012-12-23 13:24:55 -0500

    Graphs!

---
+++ class.rid.php
@@ -0,0 +1,187 @@
+<?
+
+class RID {
+
+    public $tree;
+    
+    function __construct($sauce) {
+        $this->load_dictionary($sauce);
+    }
+
+    public function alphabetize(&$tree, &$node, $letter) {
+        $tree[$letter][] = $node;
+    }
+
+    public function load_dictionary($file) {
+    
+        $linebyline = explode("\n", $file);
+        
+        $dictionary = new DOMDocument;
+        $dictionary->formatOutput = true;
+        
+        foreach ($linebyline as $line) {
+            
+            for($t = 3; $t > -1; $t--) {
+                if (preg_match("/^(\t{".$t."}\w)/", $line)) 
+                    $tabs = $t;
+            }
+            
+            //EXAMPLE* (1) --> EXAMPLE.*
+            $word = preg_replace(
+                array( "/(\(1\))|\s/", "/\*/" ),
+                array( "", ".*" ),
+                $line);
+            
+            switch ($tabs) {
+                case 0 :
+                    $node = $dictionary->createElement($word);
+                    $primary = $dictionary->appendChild($node);
+                    $primary->setAttribute('level', 'primary');
+                    
+                    break;
+                
+                case 1 :
+                    $node = $dictionary->createElement($word);
+                    $secondary = $primary->appendChild($node);
+                    $secondary->setAttribute('level', 'secondary');
+                    
+                    break;
+                
+                case 2 :
+                    if ( preg_match('/\*|(\(1\))/', $line ) ) {
+                        $node = $dictionary->createElement('term', $word);
+                        $term = $secondary->appendChild($node);
+                        $this->alphabetize($this->tree->alpha, $term, substr($word, 0, 1));
+                    }
+                    
+                    else {
+                        $node = $dictionary->createElement($word);
+                        $tertiary = $secondary->appendChild($node);
+                        $tertiary->setAttribute('level', 'tertiary');  
+                    }
+                    
+                    break;
+                
+                case 3 :
+                    $node = $dictionary->createElement('term', $word);
+                    $term = $tertiary->appendChild($node);
+                    $this->alphabetize($this->tree->alpha, $term, substr($word, 0, 1));
+                    
+                    break;
+            }
+        }
+        
+        return $this->tree->dictionary = $dictionary;
+    
+    }
+    
+    
+    public function all_parents($domElement, $function) {                                    
+        
+        if ($parent = $domElement->parentNode) {
+            
+            //As long as the top element isn't the document
+            if ($parent->nodeName != "#document")
+                $this->$function($parent);
+                
+            return $this->all_parents($parent, $function);
+            
+        }
+
+    }
+    
+    public function log_count($node) {
+        $count = $node->getAttribute('count') ? $node->getAttribute('count') : 0;
+        $node->setAttribute('count', $count + 1);
+    }
+    
+    
+    public function get_category($word) {
+    
+        $first_letter = substr($word, 0, 1);
+        $first_letter = ctype_upper($first_letter) ? $first_letter : strtoupper($first_letter) ;
+    
+        $tags = $this->tree->alpha[$first_letter];
+
+        foreach ($tags as $term)
+            if (preg_match('/^(' . $term->nodeValue . ')$/i', $word))
+                return $term;
+        
+    }
+        
+    public function analyze($input) {
+        
+        $tokens = preg_split("/[^A-z]/", $input);
+                
+        for ($i=0, $tl = count($tokens); $tl > $i; $i++)
+            if ($tokens[$i] == '')
+                unset($tokens[$i]);
+                
+        $this->words_input = count($tokens);
+        $this->words_analyzed = 0;
+        $this->input_text = $input;
+
+        foreach ($tokens as $token)
+            
+            if ($this->get_category($token)) {
+                $node = $this->get_category($token);
+                $this->words_analyzed++;
+                $this->all_parents($node, 'log_count');
+            }
+            
+        if ($this->words_analyzed > 0) return true;
+    }
+    
+    public function retrieve_data( $nodepath = array() ) {
+        $xpath = new DOMXpath($this->tree->dictionary);
+        $path = is_array($nodepath) ? "/" . implode($nodepath, "/") : "/*";
+        $query = $path . "/*[@count]";
+        
+        $nodes = $xpath->query($query);
+        
+        $data['columns'] = array(
+            array(
+                'name' => 'Category',
+                'type' => 'string'),
+            array(
+                'name' => 'Count',
+                'type' => 'number')
+            );
+        
+        foreach($nodes as $node) {
+            $data['rows'][] = array($node->nodeName, $node->getAttribute('count'));
+        }
+        
+        return $data;
+                
+    }
+    
+    public function make_data($object, $array) {
+        
+        extract($array);
+        $columncount = count($columns) - 1;
+        
+        foreach ($columns as $column)
+            echo $object . ".addColumn('" . $column['type'] . "', '" . $column['type'] . "');";
+        
+        echo $object . ".addRows([";
+
+        foreach ($rows as $row) {
+            echo "[";
+                for ($i = 0; $i <= $columncount; $i++) {
+                
+                    if ($i < $columncount)
+                        $cell = $columns[$i]['type'] == 'number' ? $row[$i]."," : "'" . $row[$i] . "'," ;
+                    
+                    else $cell = $columns[$i]['type'] == 'number' ? $row[$i] : "'" . $row[$i] . "'" ;
+                    
+                    echo $cell;
+                }
+            echo "],";
+        }
+
+        echo "]);";
+    }
+    
+
+}
--- index.php
+++ index.php
@@ -1,145 +1,45 @@
-<?
+<? 
+    include('class.rid.php'); 
+    $stats = new RID(file_get_contents('RID.CAT'));
+    $stats->analyze(file_get_contents('friends-pilot-full.txt'));
+    $data = $stats->retrieve_data(array('PRIMARY'));
+?>
+<html>
+  <head>
+    <!--Load the AJAX API-->
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
 
-class RID {
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart']});
 
-    public $tree;
-    public $words_analyzed;
-    
-    function __construct($sauce) {
-        $this->load_dictionary($sauce);
-    }
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
 
-    public function load_dictionary($file) {
-    
-        $linebyline = explode("\n", $file);
-        
-        $dictionary = new DOMDocument;
-        $dictionary->formatOutput = true;
-        
-        function alphabetize(&$tree, &$node, $letter) {
-            $tree[$letter][] = $node;
-        }
-        
-        foreach ($linebyline as $line) {
-            
-            for($t = 3; $t > -1; $t--) {
-                if (preg_match("/^(\t{".$t."}\w)/", $line)) 
-                    $tabs = $t;
-            }
-            
-            //EXAMPLE* (1) --> EXAMPLE.*
-            $word = preg_replace(
-                array( "/(\(1\))|\s/", "/\*/" ),
-                array( "", ".*" ),
-                $line);
-            
-            switch ($tabs) {
-                case 0 :
-                    $node = $dictionary->createElement($word);
-                    $primary = $dictionary->appendChild($node);
-                    $primary->setAttribute('level', 'primary');
-                    
-                    break;
-                
-                case 1 :
-                    $node = $dictionary->createElement($word);
-                    $secondary = $primary->appendChild($node);
-                    $secondary->setAttribute('level', 'secondary');
-                    
-                    break;
-                
-                case 2 :
-                    if ( preg_match('/\*|(\(1\))/', $line ) ) {
-                        $node = $dictionary->createElement('term', $word);
-                        $term = $secondary->appendChild($node);
-                        alphabetize($this->tree->alpha, $term, substr($word, 0, 1));
-                    }
-                    
-                    else {
-                        $node = $dictionary->createElement($word);
-                        $tertiary = $secondary->appendChild($node);
-                        $tertiary->setAttribute('level', 'tertiary');  
-                    }
-                    
-                    break;
-                
-                case 3 :
-                    $node = $dictionary->createElement('term', $word);
-                    $term = $tertiary->appendChild($node);
-                    alphabetize($this->tree->alpha, $term, substr($word, 0, 1));
-                    
-                    break;
-            }
-        }
-        
-        return $this->tree->dictionary = $dictionary;
-    
-    }
-    
-    
-    public function count_category($domElement) {                                    
-        if ($parent = $domElement->parentNode) {
-            
-            //As long as the top element isn't the document
-            if ($parent->nodeName != "#document") {
-                
-                $count = $parent->getAttribute('count') ? $parent->getAttribute('count') : 0;
-                $parent->setAttribute('count', $count + 1);
-                                
-            }
-            return $this->count_category($parent);
-        }
-    }
-    
-    public function get_category($word) {
-    
-        $first_letter = substr($word, 0, 1);
-        $first_letter = ctype_upper($first_letter) ? $first_letter : strtoupper($first_letter) ;
-    
-        $tags = $this->tree->alpha[$first_letter];
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
 
-            foreach ($tags as $term)  {
-            if (preg_match('/^(' . $term->nodeValue . ')$/i', $word))
-                return $term;
-            }
+        // Create the data table.
+        var data = new google.visualization.DataTable();
         
-    }
-        
-    public function analyze($input) {
-        
-        $this->words_analyzed = 0;
-        $this->input_text = $input;
-        
-        $tokens = preg_split("/[^A-z]/", $input);
-        
-        for ($i=0, $tl = count($tokens); $tl > $i; $i++)
-            if ($tokens[$i] == '')
-                unset($tokens[$i]);
+        <?php $stats->make_data( 'data', $data ); ?>
 
-        foreach ($tokens as $token)
-            
-            if ($this->get_category($token)) {
-                $node = $this->get_category($token);
-                $this->words_analyzed++;
-                $this->count_category($node);
-            }
-            
-        if ($this->words_analyzed > 0) return true;
-    }
-    
-    public function spew() {
-        $xpath = new DOMXpath($this->tree->dictionary);
-        $query = "//*[@count]";
-        $counts = $xpath->query($query);
-        foreach($counts as $count) {
-            echo $count->nodeName . " - " . $count->getAttribute('count') . "<br />";
-        }
-    }
-    
+        // Set chart options
+        var options = {'title':'Emotional Linguistics in the first Friends Episode',
+                       'width':1000,
+                       'height':600};
 
-}
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
 
-$rid = new RID(file_get_contents('RID.CAT'));
-if ($rid->analyze(file_get_contents('obama-speech.txt'))) {
-    $rid->spew();
-}
+  <body>
+    <!--Div that will hold the pie chart-->
+    <div id="chart_div" style="width:1000px; margin:0 auto;"></div>
+  </body>
+</html>
