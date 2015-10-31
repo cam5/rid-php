69aa96fe0b9ed4ab12e92e31f4611c552da5bb57
Cameron Hurd <me@cameronhurd.com>
2015-10-30 22:28:58 -0400

    Better accuracy with @covers. Let's be real here.

--- Tests/DictionaryTest.php
+++ Tests/DictionaryTest.php
@@ -6,6 +6,9 @@ use Cam5\RidPhp\Service\Dictionary;
 
 class DictionaryTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @covers \Cam5\RidPhp\Service\Dictionary::clearTemporaryValues
+     */
     public function testClearTemporaryValues()
     {
         $dictionary = new Dictionary();
