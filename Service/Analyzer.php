0425714eecd0f80cdace16868eb8850a7753b756
BlackChrRaw <38358287+BlackChrRaw@users.noreply.github.com>
2018-11-24 03:30:53 +0100

    Bugfixed and features
    - Fixed a few PHP errors and made the code work again.
    - Subcategories are accessable in analyze::retrieve_data.
    - Changed some method access restrictions to private in order to avoid confusion

--- Service/Analyzer.php
+++ Service/Analyzer.php
@@ -10,24 +10,22 @@ class Analyzer
 
     public function __construct()
     {
+		$this->tree = new \stdClass();
         $this->tree->dictionary = new Dictionary();
     }
 
-    public function all_parents($domElement, $function)
+    private function analyze_all_parents(&$domElement)
     {
-        if ($parent = $domElement->parentNode) {
-
+		$parent = $domElement->parentNode;
+        if ($parent) {
             //As long as the top element isn't the document
             if ($parent->nodeName != "#document")
-                $this->$function($parent);
-
-            return $this->all_parents($parent, $function);
-
+                $this->log_count($parent);
+            return $this->analyze_all_parents($parent );
         }
-
     }
-
-    public function log_count($node)
+	
+    public function log_count(&$node)
     {
         $count = $node->getAttribute('count') ? $node->getAttribute('count') : 0;
         $node->setAttribute('count', $count + 1);
@@ -39,13 +37,14 @@ class Analyzer
         $first_letter = substr($word, 0, 1);
         $first_letter = ctype_upper($first_letter) ? $first_letter : strtoupper($first_letter) ;
 
-        // $tags = $this->tree->alpha[$first_letter];
-
-        //foreach ($tags as $term) {
-        //    if (preg_match('/^(' . $term->nodeValue . ')$/i', $word)) {
-        //        return $term;
-        //    }
-        //}
+		$tags = $this->tree->dictionary->getTermsByLetter($first_letter);
+		
+        foreach ($tags as $term) {
+			
+            if (preg_match('/^(' . $term->nodeValue . ')$/i', $word)) {
+                return $term;
+            }
+        }
 
     }
 
@@ -60,26 +59,25 @@ class Analyzer
         $this->words_input = count($tokens);
         $this->words_analyzed = 0;
         $this->input_text = $input;
-
-        foreach ($tokens as $token)
-
-            if ($this->get_category($token)) {
-                $node = $this->get_category($token);
-                $this->words_analyzed++;
-                $this->all_parents($node, 'log_count');
+		
+        foreach ($tokens as $token){
+			$node = $this->get_category($token);
+			if ($node) {
+				$this->words_analyzed++;
+                $this->analyze_all_parents($node);
             }
-
+		}
         if ($this->words_analyzed > 0) return true;
     }
 
     public function retrieve_data( $nodepath = array() )
     {
-        $xpath = new \DOMXpath($this->tree->dictionary);
+        $xpath = new \DOMXpath($this->tree->dictionary->records);
         $path = is_array($nodepath) ? "/" . implode($nodepath, "/") : "/*";
-        $query = $path . "/*[@count]";
-
-        $nodes = $xpath->query($query);
-
+        $query = '/'.$path . "/*[@count]";
+		
+		$nodes = $xpath->query($query);
+		
         $data['columns'] = array(
             array(
                 'name' => 'Category',
@@ -88,12 +86,10 @@ class Analyzer
                 'name' => 'Count',
                 'type' => 'number')
             );
-
+		
         foreach ($nodes as $node) {
             $data['rows'][] = array($node->nodeName, $node->getAttribute('count'));
         }
-
         return $data;
-
-    }
+    }	
 }
